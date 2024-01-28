36502cf2570b2df2eabefc0b0215619039c52dc9
AndrijaNikolic0 <andrijanikoliciteh@gmail.com>
2024-01-29 00:12:09 +0100

    Kreirane putanje za usera

--- hotel-booking-laravel/routes/api.php
+++ hotel-booking-laravel/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthorizationController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+
+
+Route::post('/register', [AuthorizationController::class, 'register']);
+Route::post('/login', [AuthorizationController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+
+    Route::post('/logout', [AuthorizationController::class, 'logout']);
 });
